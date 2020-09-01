78a3fa09418a9c240bbdc12d705a4b2238ce97a9
nawan44 <rachmat.d.gunawan@gmail.com>
2020-08-31 17:00:53 -0700

    basic aritmath

---
+++ operator_aritmatic.r
@@ -0,0 +1,27 @@
+# Addition
+5+4
+## [1] 9
+
+# Substraction
+5-1
+## [1] 4
+
+# Multiplication
+6*3
+## [1] 18
+
+# Division
+5/2
+## [1] 2.5
+
+# Modulus
+5%%3
+## [1] 2
+
+# Integer
+5%/%3
+## [1] 1
+
+
+
+
